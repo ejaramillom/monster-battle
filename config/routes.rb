58c9fc5e1ce347a3229a00676f3d987e4a572a43
Juan Rojas <jrojas@fullstacklabs.co>
2023-08-22 23:27:21 -0500

    Initial Setup

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "monsters#index"
+  resources :monsters do
+    collection do
+      post :import
+    end
+  end
+end
