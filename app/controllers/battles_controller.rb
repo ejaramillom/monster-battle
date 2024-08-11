fbeb271e9ed6691ff92740a0d02e51d92f67bea0
Emmanuel <ejaramilo@hotmail.com>
2024-08-11 16:12:57 -0500

    feat(monsters): Add show battle in battles controller

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,13 +1,22 @@
 class BattlesController < ApplicationController
+  before_action :set_battle, only: %i[show]
   def index
-      @battles = Battle.all
+    @battles = Battle.all
   end
 
-  def show; end
+  def show
+    render json: { message: @battle }, status: :ok
+  end
 
   def create; end
 
   def update; end
 
   def delete; end
+
+  private
+
+  def set_battle
+    @battle = Battle.find(params[:id])
+  end
 end
